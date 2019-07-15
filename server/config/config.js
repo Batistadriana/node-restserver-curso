f89ff2d50b5f3e4c5707efe12d430ef33cf9ba14
Batistadriana <batistadriana@hotmail.com>
2019-07-15 10:39:08 -0500

    primer commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+//process objeto global que corre a lo largo de toda la aplicación de node y se actualiza dependiendo del enviroment
+
+//=============
+// Puerto
+//=============
+
+process.env.PORT = process.env.PORT || 3000;
+ 
