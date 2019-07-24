12cdb00bbf0620cc4bb93222e105eddfc1b27572
Batistadriana <batistadriana@hotmail.com>
2019-07-24 13:46:43 -0500

    Conexión Mongo Atlas con la aplicación Node

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 app.use( require('./routes/usuario')); //importamos y usamos las rutas del usuario
 
-  mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err, res)=>{
+  mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true}, (err, res)=>{
    
   if (err) throw err;
 
